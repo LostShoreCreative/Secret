fd2d6dfee4b5fb9a307877dab7fe90f066720011
Peter Stroops <internerd005@gmail.com>
2024-09-19 09:20:17 -0600

    project 2 done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Input your grade:");
+        int grade = int.Parse(Console.ReadLine());
+        string lgrade;
+        if(grade >= 90) lgrade = "A";
+        else if(grade >= 80) lgrade = "B";
+        else if(grade >= 70) lgrade = "C";
+        else if(grade >= 60) lgrade = "D";
+        else lgrade = "F"; 
+        Console.WriteLine($"Your grade is a/an {lgrade}");
     }
 }
